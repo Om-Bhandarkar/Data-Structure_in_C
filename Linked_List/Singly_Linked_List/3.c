2fbbe1b5abc4906ec53d5010910fcea3cef332d2
Om-Bhandarkar <ombhandarkar1206@gmail.com>
2023-08-19 02:22:08 +0530

    user input

---
+++ Linked_List/Singly_Linked_List/3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct College {
+    char branch[20];
+    int stud_id;
+    float cgpa;
+    struct College *next;
+};
+struct College *head = NULL;
+
+void addNode(){
+    struct College *newNode = (struct College *)malloc(sizeof(struct College));
+    printf("Enter your Branch : ");
+    fgets(newNode->branch,20,stdin);
+    printf("Enter the Student ID : ");
+    scanf("%d",&newNode->stud_id);
+    printf("Enter your cgpa : ");
+    scanf("%f",&newNode->cgpa);
+    getchar();
+    newNode->next = NULL;
+    if(head == NULL){
+        head = newNode;
+    }else{
+        struct College *temp = head;
+        while(temp -> next != NULL){
+            temp = temp -> next;
+        }
+        temp -> next = newNode;
+    }
+}
+void printLL(){
+    struct College *temp = head;
+    while(temp != NULL){
+        printf("%s ",temp->branch);
+        printf(" %d ",temp->stud_id);
+        printf(" %f ",temp->cgpa);
+        temp = temp -> next;
+    }
+    printf("\n");
+}
+int main() {
+    addNode();
+    addNode();
+    addNode();
+    printLL();
+    return 0;
+}
